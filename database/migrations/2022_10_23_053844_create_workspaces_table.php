857d8df82a18da48a88eb96bcf7aa8d5dc9d85c6
Hafiz <hafizhadi581@gmail.com>
2022-10-23 14:01:37 +0800

    create migration workspaces

--- database/migrations/2022_10_23_053844_create_workspaces_table.php
+++ database/migrations/2022_10_23_053844_create_workspaces_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('workspaces', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
