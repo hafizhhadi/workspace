82017fc0d1813a0934a4122200a983c935add51c
Hafiz <hafizhadi581@gmail.com>
2022-11-13 18:41:13 +0800

    add increments to id, add ondelete for foreign key in workspace migration

--- database/migrations/2022_10_23_053844_create_workspaces_table.php
+++ database/migrations/2022_10_23_053844_create_workspaces_table.php
@@ -14,11 +14,13 @@ return new class extends Migration
     public function up()
     {
         Schema::create('workspaces', function (Blueprint $table) {
-            $table->id();
-            $table->unsignedInteger('user_id');
+            $table->increments('id');
+            $table->unsignedInteger('user_id')->nullable();
             $table->string('name');
             $table->string('description');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
         });
     }
 
