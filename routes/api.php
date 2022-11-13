56bd346303e8edeed4c6131f94545d0268622282
Hafiz <hafizhadi581@gmail.com>
2022-11-13 18:43:18 +0800

    route for api updated

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/workspace/index', [App\Http\Controllers\User\WorkspaceController::class, 'all']);
+Route::post('/workspace/store', [App\Http\Controllers\User\WorkspaceController::class, 'storeApi']);
