512a5bca2323b8052cad835f06041f9782bbbc51
Hafiz <hafizhadi581@gmail.com>
2022-11-14 00:21:28 +0800

    wip : remove uncessary function

--- app/Http/Controllers/User/WorkspaceController.php
+++ app/Http/Controllers/User/WorkspaceController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\User;
 
+use App\Models\Task;
 use App\Models\Workspace;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
@@ -9,85 +10,65 @@ use Illuminate\Support\Facades\Auth;
 
 class WorkspaceController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function index()
     {
-       //
+        $workspaces = Workspace::all();
+        return view('workspaces.index', compact('workspaces'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         //
     }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $workspace = new Workspace();
         $workspace->user_id = Auth::id();
         $workspace->name = $request->name;
         $workspace->description = $request->description;
-
         $workspace->save();
         return redirect()->route('home');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show(Workspace $workspace)
     {
-        return view('workspaces.show', compact('workspace'));
+        // $tasks = Task::where('id',$workspace->id)->get();
+        $tasks = $workspace->tasks;
+        dd($tasks);
+        return view('workspaces.show', compact('workspace','tasks'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request, $id)
     {
         //
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         //
+    }
+
+    public function all()
+    {
+        $workspace = Workspace::all();
+
+        return response()->json([
+            'message' => 'Success',
+            'data' => $workspace,
+        ]);
+        
+    }
+    public function storeApi(Request $request)
+    {
+        $workspace = Workspace::create([ //mass assign
+            'user_id' => Auth::id(),
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return response()->json([
+            'data' => $workspace,
+            'message' => 'Berjaya create',
+        ]);
     }
 }
