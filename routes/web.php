d3ef34232801253cd182d42954db2e4c9e90566b
Hafiz <hafizhadi581@gmail.com>
2022-11-13 18:43:34 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,13 @@ Route::get('/', function () {
 
 Auth::routes();
 
+//Home
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::post('/workspace/store', [App\Http\Controllers\User\WorkspaceController::class, 'store'])->name('workspace:store');
+Route::get('/workspace/show/{workspace}', [App\Http\Controllers\User\WorkspaceController::class, 'show'])->name('workspace:show');
+
+
+Route::post('/task/store/{workspace}', [App\Http\Controllers\User\TaskController::class, 'store'])->name('task:store');
+// Route::get('/home/show/{task}', [App\Http\Controllers\User\TaskController::class, 'show'])->name('task:show');
