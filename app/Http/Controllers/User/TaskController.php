cb661d7f10baaa3b135270b2c35f4fbee3442d0f
Hafiz <hafizhadi581@gmail.com>
2022-11-13 18:38:33 +0800

    create task controller, add store function

---
+++ app/Http/Controllers/User/TaskController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Models\Task;
+use App\Models\Workspace;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request, Workspace $workspace)
+    {
+        $task = new Task();
+        $task->workspace_id = $workspace->id;
+        $task->name = $request->name;
+        // $task->status = $request->status;
+        $task->description = $request->description;
+        $task->date = $request->due_date." ".$request->due_time;
+        $task->save();
+        return redirect()->route('workspace:show', compact('workspace'));
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Task $task)
+    {
+        return view ('workspaces.show', compact('task'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
