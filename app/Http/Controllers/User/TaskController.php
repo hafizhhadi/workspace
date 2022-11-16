d433fe3e9d3d76c5da048f914ad7040ae3861dd0
Hafiz <hafizhadi581@gmail.com>
2022-11-16 14:58:42 +0800

    create store for task using mass assign

--- app/Http/Controllers/User/TaskController.php
+++ app/Http/Controllers/User/TaskController.php
@@ -9,86 +9,17 @@ use App\Http\Controllers\Controller;
 
 class TaskController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request, Workspace $workspace)
     {
-        $task = new Task();
-        $task->workspace_id = $workspace->id;
-        $task->name = $request->name;
-        // $task->status = $request->status;
-        $task->description = $request->description;
-        $task->date = $request->due_date." ".$request->due_time;
-        $task->save();
-        return redirect()->route('workspace:show', compact('workspace'));
+        Task::create([
+            'workspace_id' => $workspace->id,
+            'user_id' => auth()->user()->id,
+            'name' => $request->name,
+            'description' => $request->description,
+            'date' => $request->due_date." ".$request->due_time,
+        ]);
+     
+        return view('workspaces.show', compact('workspace'));
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Task $task)
-    {
-        return view ('workspaces.show', compact('task'));
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-    }
 }
